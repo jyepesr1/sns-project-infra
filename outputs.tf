eeada558dd3b4a57df62d0610204237543198294
Johan Yepes <jyepesr@psl.com.co>
2019-11-25 12:30:33 -0500

    Adding ALB dns name and SNS Topic ARN to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "instance_ip_addr" {
   value = aws_ecr_repository.sns_ecr_repo.repository_url
 }
+
+output "alb_dns_name" {
+  value = aws_alb.alb.dns_name
+}
+
+output "topic_arn" {
+  value = aws_sns_topic.email_topic.arn
+}
